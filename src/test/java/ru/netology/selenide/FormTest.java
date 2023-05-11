995dfc146a450b264122f566bc387b8061b53f0b
Ekaterina <katerina331-441@ya.ru>
2023-05-11 21:53:43 +0300

    Edit test

--- src/test/java/ru/netology/selenide/FormTest.java
+++ src/test/java/ru/netology/selenide/FormTest.java
@@ -1,26 +1,28 @@
 package ru.netology.selenide;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-
-import java.sql.Date;
-import java.text.SimpleDateFormat;
+import org.openqa.selenium.Keys;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class FormTest {
+    public String newDatePlusFormat(int plusDay, String dateFormat) {
+        return LocalDate.now().plusDays(plusDay).format(DateTimeFormatter.ofPattern(dateFormat));
+    }
+
     @Test
     void shouldTestV1() {
         Configuration.holdBrowserOpen = false;
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Воронеж");
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        String date = dateFormat.format(Date.valueOf(LocalDate.now().plusDays(4)));
-        $("[data-test-id=date] input").doubleClick().sendKeys(date);
+        $("[data-test-id=date] input").doubleClick().sendKeys(newDatePlusFormat(4, "dd.MM.yyyy"));
         $("[data-test-id=name] input").setValue("Васильев-Иванов Василий");
         $("[data-test-id=phone] input").setValue("+78002225577");
         $("[data-test-id=agreement]").click();
@@ -34,13 +36,11 @@ public class FormTest {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Во");
         $x("//*[contains(text(),'Воронеж')]").click();
-        LocalDate date = LocalDate.now().plusDays(4);
-        $("[data-test-id=date] input").click();
-        $x("//table //*[contains(text(), '" + date.getDayOfMonth() + "')]").click();
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.ARROW_DOWN, Keys.ARROW_RIGHT, Keys.ENTER);
         $("[data-test-id=name] input").setValue("Васильев-Иванов Василий");
         $("[data-test-id=phone] input").setValue("+78002225577");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-        $("[data-test-id=notification]").should(visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification]").shouldHave(Condition.text("Встреча успешно забронирована на " + newDatePlusFormat(4, "dd.MM.yyyy")), Duration.ofSeconds(15)).should(visible);
     }
 }
